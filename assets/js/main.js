ffd223746fcc5ed78e5a5b808c7f8a9e2b1ecbdf
Anna KC <29369408+kania0507@users.noreply.github.com>
2026-02-10 13:21:32 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,7 +45,7 @@ const Project = { template: `
     <router-link v-if="propObj" :to="{name: 'ProjectDetails', path: '/projects/'+ propObj.id, params: {id: propObj.id, propObj:propObj, prjCount:prjCount, allProjects: cat === 'all' ? allProjects : allProjects.filter(el => el.category.includes(cat)), cat: cat } }">
         <div class="inbox">
             <h1>{{ propObj.name }} </h1>  
-            <p v-if="propObj.small_photo !== ''" :class="{ hide: propObj.small_photo == ''}" > <img  v-if="propObj.small_photo != ''"  :src="'./assets/img/'+propObj.small_photo"  class="img-responsive" /></p>
+            <p v-if="propObj.photo_small !== ''" :class="{ hide: propObj.photo_small == ''}" > <img  v-if="propObj.photo_small != ''"  :src="'./assets/img/'+propObj.photo_small"  class="img-responsive" /></p>
             <p v-else-if="propObj.photo !== ''" :class="{ hide: propObj.photo == ''}" > <img  v-if="propObj.photo != ''"  :src="'./assets/img/'+propObj.photo"  class="img-responsive" /></p>
             <p v-if="cat"><span class="box-cat" v-for="cat in propObj.category"> {{ cat }}</span></p>
         </div>
